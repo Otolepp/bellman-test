6c0cea1f5581507356b64b2d1beee24e2f6853f5
Otolepp <guillaumebiquet94@gmail.com>
2021-07-01 10:51:24 +0200

    import font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function App() {
   return (
     <div className="App">
+      <FontAwesomeIcon icon={faHome}/>
       <p> Hello </p>
     </div>
   );
